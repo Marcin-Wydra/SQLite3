91e0fa1879fa8f33580e238e78de40a54e2faf14
Marcin-Wydra <marcinwydra94@gmail.com>
2026-01-18 23:36:35 +0100

    refactored to object code

--- db.py
+++ db.py
@@ -1,28 +1,41 @@
 import sqlite3
 
+class Database:
+    def __init__(self, path):
+        self.con - sqlite3.connect(path)
+
 def connect_to_db(path):
     conn = sqlite3.connect(path)
     return conn
 
-def create_table(conn):
+def create_table(self):
     query = "CREATE TABLE IF NOT EXISTS Customers(id INTEGER PRIMARY KEY, \
              name TEXT NOT NULL, surname TEXT NOT NULL, date_joined DATETIME NOT NULL);"
-    conn.execute(query)
-    conn.commit()
+    self.con.execute(query)
 
-def add_to_customers(conn, name, surname, date_joined):
+def add_to_customers(self, name, surname, date_joined):
     query = "INSERT INTO customers(name, surname, date_joined) VALUES (?, ?, ?)"
-    conn.execute(query, (name, surname, date_joined))
-    conn.commit()
+    self.con.execute(query, (name, surname, date_joined))
 
-conn = connect_to_db("example-database.sqlite3")
-create_table(conn)
-add_to_customers(conn, 'John', 'Wick', '2000-09-02')
-add_to_customers(conn, 'James', 'Bond', '2002-05-16')
-
-def preview_table(conn, table_name):
+def preview_table(self, table_name):
     query = f'SELECT * FROM {table_name}'
-    result = conn.execute(query).fetchall()
+    result = self.con.execute(query).fetchall()
     print(result)
 
-preview_table(conn, 'Customers')
+def __enter__(self):
+    return self
+
+def __exit__(self, exc_type, exc_value, traceback):
+    if isinstance(exc_type, Exception):
+        self.con.rollback()
+    else:
+        self.con.commit()
+
+    self.con.close()
+
+with Database('example2-data.db') as db:
+    db.create_table()
+    db.add_to_customers('John', 'Wick', '2000-09-02')
+    db.add_to_customers('James', 'Bond', '2002-05-16')
+    db.preview_table('Customers')
+
