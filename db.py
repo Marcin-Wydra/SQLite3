143c4f90553b84ebe11d062e83dccfc853f9183c
Marcin-Wydra <marcinwydra94@gmail.com>
2026-01-19 00:41:20 +0100

    reformate code

--- db.py
+++ db.py
@@ -4,42 +4,42 @@ class Database:
     def __init__(self, path):
         self.con = sqlite3.connect(path)
 
-def connect_to_db(path):
-    conn = sqlite3.connect(path)
-    return conn
+    def connect_to_db(path):
+        conn = sqlite3.connect(path)
+        return conn
 
-def create_table(self):
-    query = "CREATE TABLE IF NOT EXISTS Customers(id INTEGER PRIMARY KEY, \
+    def create_table(self):
+        query = "CREATE TABLE IF NOT EXISTS Customers(id INTEGER PRIMARY KEY, \
              name TEXT NOT NULL, surname TEXT NOT NULL, date_joined DATETIME NOT NULL);"
-    self.con.execute(query)
+        self.con.execute(query)
 
-def add_to_customers(self, name, surname, date_joined):
-    query = "INSERT INTO customers(name, surname, date_joined) VALUES (?, ?, ?)"
-    self.con.execute(query, (name, surname, date_joined))
+    def add_to_customers(self, name, surname, date_joined):
+        query = "INSERT INTO customers(name, surname, date_joined) VALUES (?, ?, ?)"
+        self.con.execute(query, (name, surname, date_joined))
 
-def preview_table(self, table_name):
-    query = f'SELECT * FROM {table_name}'
-    result = self.con.execute(query).fetchall()
-    print(result)
+    def preview_table(self, table_name):
+        query = f'SELECT * FROM {table_name}'
+        result = self.con.execute(query).fetchall()
+        print(result)
 
-def delete_from_customers(self, id):
-    query = 'DELETE FROM customers WHERE id = ?'
-    self.con.execute(query, (id, ))
+    def delete_from_customers(self, id):
+        query = 'DELETE FROM customers WHERE id = ?'
+        self.con.execute(query, (id, ))
 
-def update_customers(self, id, name, surname, date_joined):
-    query = 'UPDATE customers SET name = ?, surname = ?, date_joined = ? WHERE id = ?'
-    self.con.execute(query, (name, surname, date_joined, id))
+    def update_customers(self, id, name, surname, date_joined):
+        query = 'UPDATE customers SET name = ?, surname = ?, date_joined = ? WHERE id = ?'
+        self.con.execute(query, (name, surname, date_joined, id))
 
-def __enter__(self):
-    return self
+    def __enter__(self):
+        return self
 
-def __exit__(self, exc_type, exc_value, traceback):
-    if isinstance(exc_type, Exception):
-        self.con.rollback()
-    else:
-        self.con.commit()
+    def __exit__(self, exc_type, exc_value, traceback):
+        if isinstance(exc_type, Exception):
+            self.con.rollback()
+        else:
+            self.con.commit()
 
-    self.con.close()
+        self.con.close()
 
 with Database('example2-data.db') as db:
     db.create_table()
